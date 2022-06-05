e6f619a2e1f2b1bbc514b88b06d6aa711ff21d08
BlackHeart32 <eguzmandls@gmail.com>
2022-06-05 12:18:15 -0400

    Front end spa implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, map, Observable, of, startWith } from 'rxjs';
+import { DataState } from './enum/data-state.enum';
 import { AppState } from './interface/app-state';
 import { CustomResponse } from './interface/custom-response';
+import { ItemService } from './service/item.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  // appState$: Observable<AppState<CustomResponse>>;
+export class AppComponent implements OnInit {
+  appState$: Observable<AppState<CustomResponse>>;
 
-  // constructor(private )
+  constructor(private itemService: ItemService){}
+
+  ngOnInit(): void{
+    this.appState$= this.itemService.items$
+      .pipe(
+        map(response => {
+          return {dataState: DataState.LOADED_STATE, appData: response}
+        }),
+        startWith({dataState: DataState.LOADING_STATE}),
+        catchError((error: string) =>{
+          return of({dataState: DataState.ERROR_STATE, error})
+        })
+      )
+  }
 }
--- src/app/service/item.service.ts
+++ src/app/service/item.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { catchError, Observable, Subscriber, tap, throwError } from 'rxjs';
 import { CustomResponse } from '../interface/custom-response';
-import {HttpClient, HttpErrorResponse} from '@angular/common/http'
+import { HttpClient, HttpErrorResponse } from '@angular/common/http'
 import { Item } from '../interface/item';
 import { Status } from '../enum/status.enum';
 @Injectable({
@@ -15,33 +15,46 @@ export class ItemService {
 
   items$ = <Observable<CustomResponse>>
     this.http.get<CustomResponse>(`${this.apiUri}/item/list`)
-    .pipe(
-      tap(console.log),
-      catchError(this.handleError)
-    )
-
-    register$ = (item: Item) => <Observable<CustomResponse>>
-    this.http.get<CustomResponse>(`${this.apiUri}/item/register`)
-    .pipe(
-      tap(console.log),
-      catchError(this.handleError)
-    )
-
-    filter$ = (status: Status, response: CustomResponse) => <Observable<CustomResponse>>
-      new Observable<CustomResponse>(
-        subscriber => {
-          console.log(response)
-          subscriber.next(
-            status === Status.ALL ? {...response, message: `Items filtered by ${status} status`}
-            : {...response, message: response.data.items
-            .filter(item => item.status === status).length > 0 ? `Items filtered by ${status ===}`}
-          )
-        }
+      .pipe(
+        tap(console.log),
+        catchError(this.handleError)
       )
 
-    private handleError(error: HttpErrorResponse): Observable<never>{
-      console.log(error)
-      return throwError (`An error has occurred - Error code: ${error.status}`)
-    }
+  register$ = (item: Item) => <Observable<CustomResponse>>
+    this.http.get<CustomResponse>(`${this.apiUri}/item/register`)
+      .pipe(
+        tap(console.log),
+        catchError(this.handleError)
+      )
+
+  filter$ = (status: Status, response: CustomResponse) => <Observable<CustomResponse>>
+    new Observable<CustomResponse>(
+      subscriber => {
+        console.log(response)
+        subscriber.next(
+          status === Status.ALL ? { ...response, message: `Items filtered by ${status} status` }
+            : {
+              ...response, message: response.data.items
+                .filter(item => item.status === status).length > 0 ? `Items filtered by 
+            ${status === Status.ON_STOCK ? 'ITEM ON STOCK' : 'ITEM UNAVAILABLE'} status` : `No items of ${status} found`,
+              data: {
+                items: response.data.items
+                  ?.filter(item => item.status === status)
+              }
+            }
+        );
+        subscriber.complete();
+      }
+    )
+      .pipe(
+        tap(console.log),
+        catchError(this.handleError)
+      )
+
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    console.log(error)
+    return throwError(`An error has occurred - Error code: ${error.status}`)
+  }
   //EOF
 }
